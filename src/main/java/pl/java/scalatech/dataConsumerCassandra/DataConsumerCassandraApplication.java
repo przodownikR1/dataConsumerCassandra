3d968a8dacb9b72151eb99dc4cff12b79c665681
slawek <przodownikr1@gmail.com>
2018-12-27 00:06:53 +0100

    init project

---
+++ src/main/java/pl/java/scalatech/dataConsumerCassandra/DataConsumerCassandraApplication.java
@@ -0,0 +1,14 @@
+package pl.java.scalatech.dataConsumerCassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConsumerCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataConsumerCassandraApplication.class, args);
+	}
+
+}
+
